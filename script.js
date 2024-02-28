a5a8ae3282e447260ab8cdf981fd3908e97c78c7
Alphamikey23 <eng22cse001@gmail.com>
2024-02-28 15:52:02 +0530

    third commit

--- script.js
+++ script.js
@@ -12,37 +12,51 @@ console.log(x/y)
 // Challenge 5: perform division operation (divide x by y) and print the remainder in console.
 console.log(x%y)
 // Challenge 6: Perform Exponent operation and print the answer in console.
-
-var z = 20;
+console.log(Math.pow(z, 2)); 
 
 // Challenge 7: calculate "x" to the power of the "y" and print the answer in console.
-
+console.log(Math.pow(x, y)); 
 // Challenge 8: Decrement the value of the variable "z" by 1 and print the z value in the console.
+z--;
+console.log(z); 
 
 var a = 20;
-
 var b = 10;
-
 var c = 15;
-
 var d = 20;
 
 // Challenge 9: Perform Addition assignment operation to the variable a;
+a += 5; 
 
-// Challenge 10: Perform 	Subtraction assignment operation to the variable b;
+// Challenge 10: Perform Subtraction assignment operation to the variable b;
+b -= 3; 
 
-// Challenge 11: Perform 	Multiplication assignment operation to the variable c;
+// Challenge 11: Perform Multiplication assignment operation to the variable c;
+c *= 2; 
 
-// Challenge 12: Perform 	Division assignment operation to the variable c;
+// Challenge 12: Perform Division assignment operation to the variable c;
+c /= 3; 
 
 // Challenge 13: Perform 2 Strict equality Comparison operations, in which one should return true and one should return false.
+console.log(d === 20); 
+console.log(a === b); 
 
 // Challenge 14: Perform 2 Strict-non-equality Comparison operations, in which one should return true and one should return false.
+console.log(a !== b); 
+console.log(c !== 10); 
 
 // Challenge 15: Perform 2 Less than Comparison operations, in which one should return true and one should return false.
+console.log(a < c); 
+console.log(b < d);
 
 // Challenge 16: Perform 2 Greater than Comparison operations, in which one should return true and one should return false.
+console.log(a > b); 
+console.log(c > d); 
 
 // Challenge 17: Perform 2 Less than or equal to Comparison operations, in which one should return true and one should return false.
+console.log(a <= 20); 
+console.log(b <= 8); 
 
 // Challenge 18: Perform 2 Greater than or equal to Comparison operations, in which one should return true and one should return false.
+console.log(c >= d); 
+console.log(b >= 12); 
